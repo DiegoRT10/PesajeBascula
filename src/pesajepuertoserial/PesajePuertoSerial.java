22f790321134eb0b535a5f3c06296e61356a7220
DiegoRT10 <alexisdieram@gmail.com>
2022-11-10 00:55:21 -0600

    Se creo un jfram y funcion de lecctura por el puerto COM2 a travez de hilos

--- src/pesajepuertoserial/PesajePuertoSerial.java
+++ src/pesajepuertoserial/PesajePuertoSerial.java
@@ -4,15 +4,8 @@
  */
 package pesajepuertoserial;
 
-import gnu.io.CommPort;
-import gnu.io.CommPortIdentifier;
-import gnu.io.PortInUseException;
-import gnu.io.SerialPort;
-import gnu.io.UnsupportedCommOperationException;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Enumeration;
-import java.util.Scanner;
+import views.Pesaje;
+
 
 /**
  *
@@ -23,48 +16,12 @@ public class PesajePuertoSerial {
     /**
      * @param args the command line arguments
      */
-    public static Double peso=0.0;
+ 
     
     public static void main(String[] args) {
-          CommPortIdentifier cpi = null;
-    SerialPort puertoSerial = null;
-    Scanner entrada = null;
-    PrintStream salida = null;
-    Enumeration comports = CommPortIdentifier.getPortIdentifiers();
-    while(comports.hasMoreElements()){
-    cpi = (CommPortIdentifier) comports.nextElement();
-    if(cpi.getName().equals("COM2")){
-        break;
-    }
-    }
-    
-        try {
-            CommPort puerto = cpi.open("Puerto Serial",2000);
-            puertoSerial = (SerialPort) puerto;
-            puertoSerial.setSerialPortParams(19200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-            entrada = new Scanner(puertoSerial.getInputStream());
-            salida = new PrintStream(puertoSerial.getOutputStream());
-        } catch (PortInUseException ex) {
-            //Logger.getLogger(pesaje.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            //Logger.getLogger(pesaje.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedCommOperationException ex) {
-            //Logger.getLogger(pesaje.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        while(!entrada.hasNextDouble()){
-            entrada.close();
-            entrada = null;
-        try {
-            entrada = new Scanner(puertoSerial.getInputStream());
-        } catch (IOException ex) {
-            //Logger.getLogger(pesaje.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        }
-        peso = entrada.nextDouble();
-        System.out.println("Peso: "+peso);
-//        salida.println("Dato capturado");
+         Pesaje p = new Pesaje();
+         p.setVisible(true);
+         
     }
     
 }
